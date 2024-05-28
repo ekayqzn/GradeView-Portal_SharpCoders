ce6769830457151dcf2d46de791f5aa71d2dcbca
evoraQzone <erikaquizon04@outlook.com>
2024-05-28 15:09:16 +0800

    Add Class for customize gradebook  queries

--- Add Section.Designer.cs
+++ Add Section.Designer.cs
@@ -50,9 +50,10 @@
             "Bachelor of Science in Information Technology",
             "Bachelor of Secondary Education major in English",
             "Bachelor of Secondary Education major in Mathematics"});
-            this.cboProgram.Location = new System.Drawing.Point(111, 54);
+            this.cboProgram.Location = new System.Drawing.Point(83, 44);
+            this.cboProgram.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.cboProgram.Name = "cboProgram";
-            this.cboProgram.Size = new System.Drawing.Size(398, 24);
+            this.cboProgram.Size = new System.Drawing.Size(300, 21);
             this.cboProgram.TabIndex = 0;
             this.cboProgram.Text = "Bachelor of Science in Information Technology";
             this.cboProgram.SelectedIndexChanged += new System.EventHandler(this.cboProgram_SelectedIndexChanged);
@@ -60,24 +61,27 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(46, 57);
+            this.label1.Location = new System.Drawing.Point(34, 46);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(59, 16);
+            this.label1.Size = new System.Drawing.Size(46, 13);
             this.label1.TabIndex = 1;
             this.label1.Text = "Program";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(46, 103);
+            this.label2.Location = new System.Drawing.Point(34, 84);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(36, 16);
+            this.label2.Size = new System.Drawing.Size(29, 13);
             this.label2.TabIndex = 2;
             this.label2.Text = "Year";
             // 
             // numYear
             // 
-            this.numYear.Location = new System.Drawing.Point(111, 103);
+            this.numYear.Location = new System.Drawing.Point(83, 84);
+            this.numYear.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.numYear.Maximum = new decimal(new int[] {
             4,
             0,
@@ -89,7 +93,7 @@
             0,
             0});
             this.numYear.Name = "numYear";
-            this.numYear.Size = new System.Drawing.Size(117, 22);
+            this.numYear.Size = new System.Drawing.Size(88, 20);
             this.numYear.TabIndex = 3;
             this.numYear.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             this.numYear.Value = new decimal(new int[] {
@@ -101,15 +105,17 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(46, 150);
+            this.label3.Location = new System.Drawing.Point(34, 122);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(52, 16);
+            this.label3.Size = new System.Drawing.Size(43, 13);
             this.label3.TabIndex = 4;
             this.label3.Text = "Section";
             // 
             // numSection
             // 
-            this.numSection.Location = new System.Drawing.Point(111, 150);
+            this.numSection.Location = new System.Drawing.Point(83, 122);
+            this.numSection.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.numSection.Maximum = new decimal(new int[] {
             2,
             0,
@@ -121,7 +127,7 @@
             0,
             0});
             this.numSection.Name = "numSection";
-            this.numSection.Size = new System.Drawing.Size(117, 22);
+            this.numSection.Size = new System.Drawing.Size(88, 20);
             this.numSection.TabIndex = 5;
             this.numSection.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             this.numSection.Value = new decimal(new int[] {
@@ -136,9 +142,10 @@
             this.rbtnOK.FlatAppearance.BorderSize = 0;
             this.rbtnOK.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.rbtnOK.ForeColor = System.Drawing.Color.White;
-            this.rbtnOK.Location = new System.Drawing.Point(568, 327);
+            this.rbtnOK.Location = new System.Drawing.Point(426, 266);
+            this.rbtnOK.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.rbtnOK.Name = "rbtnOK";
-            this.rbtnOK.Size = new System.Drawing.Size(150, 40);
+            this.rbtnOK.Size = new System.Drawing.Size(112, 32);
             this.rbtnOK.TabIndex = 6;
             this.rbtnOK.Text = "OK";
             this.rbtnOK.UseVisualStyleBackColor = false;
@@ -146,9 +153,9 @@
             // 
             // Add_Section
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
             this.Controls.Add(this.rbtnOK);
             this.Controls.Add(this.numSection);
             this.Controls.Add(this.label3);
@@ -156,9 +163,11 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.cboProgram);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Add_Section";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Add_Section";
+            this.Load += new System.EventHandler(this.Add_Section_Load);
             ((System.ComponentModel.ISupportInitialize)(this.numYear)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numSection)).EndInit();
             this.ResumeLayout(false);

1cdee6b9ae8e25400b22ff4944f0d3fe4e3759de
evoraQzone <erikaquizon04@outlook.com>
2024-06-03 16:10:47 +0800

    Added UI

--- LandingPage.Designer.cs
+++ LandingPage.Designer.cs
@@ -28,17 +28,23 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.lblSchoolName = new System.Windows.Forms.Label();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LandingPage));
             this.llblHome = new System.Windows.Forms.LinkLabel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.linkLabel1 = new System.Windows.Forms.LinkLabel();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.rbtnStudent = new gradesBookApp.RoundedButton();
+            this.rbtnFaculty = new gradesBookApp.RoundedButton();
+            this.btnAdminstrator = new gradesBookApp.RoundedButton();
+            this.roundedButton1 = new gradesBookApp.RoundedButton();
+            this.roundedButton2 = new gradesBookApp.RoundedButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             this.SuspendLayout();
             // 
@@ -68,47 +74,74 @@
             // 
             // llblHome
             // 
+            this.llblHome.ActiveLinkColor = System.Drawing.Color.Yellow;
+            this.llblHome.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.llblHome.AutoSize = true;
-            this.llblHome.BackColor = System.Drawing.Color.Navy;
+            this.llblHome.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(4)))), ((int)(((byte)(93)))));
             this.llblHome.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.llblHome.LinkColor = System.Drawing.Color.White;
-            this.llblHome.Location = new System.Drawing.Point(1199, 23);
+            this.llblHome.Location = new System.Drawing.Point(1599, 28);
+            this.llblHome.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.llblHome.Name = "llblHome";
             this.llblHome.Size = new System.Drawing.Size(56, 20);
             this.llblHome.TabIndex = 2;
             this.llblHome.TabStop = true;
             this.llblHome.Text = "Home";
+            this.llblHome.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.llblHome_LinkClicked);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(4)))), ((int)(((byte)(93)))));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(-4, -2);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(1797, 91);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // linkLabel1
             // 
+            this.linkLabel1.ActiveLinkColor = System.Drawing.Color.Yellow;
+            this.linkLabel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.linkLabel1.AutoSize = true;
-            this.linkLabel1.BackColor = System.Drawing.Color.Navy;
+            this.linkLabel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(4)))), ((int)(((byte)(93)))));
             this.linkLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.linkLabel1.LinkColor = System.Drawing.Color.White;
-            this.linkLabel1.Location = new System.Drawing.Point(1085, 23);
+            this.linkLabel1.Location = new System.Drawing.Point(1447, 28);
+            this.linkLabel1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.linkLabel1.Name = "linkLabel1";
             this.linkLabel1.Size = new System.Drawing.Size(66, 20);
             this.linkLabel1.TabIndex = 3;
             this.linkLabel1.TabStop = true;
             this.linkLabel1.Text = "Sign In";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
             // 
-            // textBox2
+            // textBox3
             // 
             this.textBox2.BackColor = System.Drawing.Color.Blue;
             this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.textBox2.Location = new System.Drawing.Point(294, 250);
+            this.textBox2.Location = new System.Drawing.Point(392, 308);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(730, 694);
+            this.textBox2.Size = new System.Drawing.Size(973, 854);
             this.textBox2.TabIndex = 8;
             // 
-            // pictureBox1
+            // pictureBox5
             // 
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.pictureBox1.Image = global::gradesBookApp.Properties.Resources.SignIn_Icon;
-            this.pictureBox1.Location = new System.Drawing.Point(1044, 13);
+            this.pictureBox1.Location = new System.Drawing.Point(1392, 16);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(37, 37);
+            this.pictureBox1.Size = new System.Drawing.Size(49, 46);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 6;
             this.pictureBox1.TabStop = false;
@@ -117,54 +150,142 @@
             // 
             this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.pictureBox2.Image = global::gradesBookApp.Properties.Resources.Home_Icon;
-            this.pictureBox2.Location = new System.Drawing.Point(1158, 13);
+            this.pictureBox2.Location = new System.Drawing.Point(1544, 16);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(37, 37);
+            this.pictureBox2.Size = new System.Drawing.Size(49, 46);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 5;
             this.pictureBox2.TabStop = false;
-            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click_1);
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click_2);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(4)))), ((int)(((byte)(93)))));
+            this.pictureBox1.Image = global::gradesBookApp.Properties.Resources.SHARP__2_;
+            this.pictureBox1.Location = new System.Drawing.Point(1625, 20);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(55, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // pictureBox3
             // 
+            this.pictureBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.pictureBox3.Image = global::gradesBookApp.Properties.Resources.school_background;
-            this.pictureBox3.Location = new System.Drawing.Point(-3, 56);
+            this.pictureBox3.Location = new System.Drawing.Point(-4, 69);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(1286, 888);
+            this.pictureBox3.Size = new System.Drawing.Size(1715, 1093);
             this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox3.TabIndex = 7;
+            this.pictureBox3.TabIndex = 19;
             this.pictureBox3.TabStop = false;
-            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click_1);
             // 
-            // textBox3
+            // rbtnStudent
+            // 
+            this.rbtnStudent.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.rbtnStudent.BackColor = System.Drawing.Color.White;
+            this.rbtnStudent.FlatAppearance.BorderSize = 0;
+            this.rbtnStudent.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.rbtnStudent.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbtnStudent.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(4)))), ((int)(((byte)(93)))));
+            this.rbtnStudent.Location = new System.Drawing.Point(1296, 537);
+            this.rbtnStudent.Name = "rbtnStudent";
+            this.rbtnStudent.Size = new System.Drawing.Size(435, 58);
+            this.rbtnStudent.TabIndex = 18;
+            this.rbtnStudent.Text = "STUDENT";
+            this.rbtnStudent.UseVisualStyleBackColor = false;
+            this.rbtnStudent.Click += new System.EventHandler(this.rbtnStudent_Click);
+            // 
+            // rbtnFaculty
+            // 
+            this.rbtnFaculty.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.rbtnFaculty.BackColor = System.Drawing.Color.White;
+            this.rbtnFaculty.FlatAppearance.BorderSize = 0;
+            this.rbtnFaculty.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.rbtnFaculty.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbtnFaculty.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(4)))), ((int)(((byte)(93)))));
+            this.rbtnFaculty.Location = new System.Drawing.Point(1296, 456);
+            this.rbtnFaculty.Name = "rbtnFaculty";
+            this.rbtnFaculty.Size = new System.Drawing.Size(435, 58);
+            this.rbtnFaculty.TabIndex = 17;
+            this.rbtnFaculty.Text = "FACULTY";
+            this.rbtnFaculty.UseVisualStyleBackColor = false;
+            this.rbtnFaculty.Click += new System.EventHandler(this.rbtnFaculty_Click);
+            // 
+            // btnAdminstrator
+            // 
+            this.btnAdminstrator.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnAdminstrator.BackColor = System.Drawing.Color.White;
+            this.btnAdminstrator.FlatAppearance.BorderSize = 0;
+            this.btnAdminstrator.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAdminstrator.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdminstrator.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(4)))), ((int)(((byte)(93)))));
+            this.btnAdminstrator.Location = new System.Drawing.Point(1296, 695);
+            this.btnAdminstrator.Name = "btnAdminstrator";
+            this.btnAdminstrator.Size = new System.Drawing.Size(435, 58);
+            this.btnAdminstrator.TabIndex = 16;
+            this.btnAdminstrator.Text = "ADMINISTRATOR";
+            this.btnAdminstrator.UseVisualStyleBackColor = false;
+            this.btnAdminstrator.Click += new System.EventHandler(this.btnAdminstrator_Click);
+            // 
+            // roundedButton1
+            // 
+            this.roundedButton1.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.roundedButton1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(4)))), ((int)(((byte)(93)))));
+            this.roundedButton1.Enabled = false;
+            this.roundedButton1.FlatAppearance.BorderSize = 0;
+            this.roundedButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.roundedButton1.ForeColor = System.Drawing.Color.Transparent;
+            this.roundedButton1.Location = new System.Drawing.Point(1202, 131);
+            this.roundedButton1.Name = "roundedButton1";
+            this.roundedButton1.Size = new System.Drawing.Size(1078, 1195);
+            this.roundedButton1.TabIndex = 32;
+            this.roundedButton1.UseVisualStyleBackColor = false;
+            // 
+            // roundedButton2
             // 
             this.textBox3.BackColor = System.Drawing.Color.Navy;
             this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.textBox3.Location = new System.Drawing.Point(351, 191);
+            this.textBox3.Location = new System.Drawing.Point(468, 235);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox3.Multiline = true;
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(617, 753);
+            this.textBox3.Size = new System.Drawing.Size(822, 926);
             this.textBox3.TabIndex = 9;
             // 
             // LandingPage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1284, 941);
+            this.ClientSize = new System.Drawing.Size(1712, 1158);
             this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.rbtnStudent);
+            this.Controls.Add(this.rbtnFaculty);
+            this.Controls.Add(this.btnAdminstrator);
+            this.Controls.Add(this.roundedButton1);
+            this.Controls.Add(this.roundedButton2);
             this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.linkLabel1);
             this.Controls.Add(this.llblHome);
-            this.Controls.Add(this.lblSchoolName);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.pictureBox3);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "LandingPage";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.LandingPage_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -172,15 +293,18 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Label lblSchoolName;
         private System.Windows.Forms.LinkLabel llblHome;
+        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.LinkLabel linkLabel1;
-        private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private RoundedButton btnAdminstrator;
+        private RoundedButton rbtnFaculty;
+        private RoundedButton rbtnStudent;
         private System.Windows.Forms.PictureBox pictureBox3;
-        private System.Windows.Forms.TextBox textBox2;
+        private RoundedButton roundedButton1;
+        private RoundedButton roundedButton2;
+        private System.Windows.Forms.PictureBox pictureBox5;
         private System.Windows.Forms.TextBox textBox3;
     }
 }

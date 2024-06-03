43d6e63659867d6ed54c469ecb628bae9d726ad8
cdefghannah <hannahdinopol@gmail.com>
2024-06-03 11:39:49 +0800

    modified

--- LandingPage.Designer.cs
+++ LandingPage.Designer.cs
@@ -32,10 +32,10 @@
             this.lblSchoolName = new System.Windows.Forms.Label();
             this.llblHome = new System.Windows.Forms.LinkLabel();
             this.linkLabel1 = new System.Windows.Forms.LinkLabel();
-            this.textBox2 = new System.Windows.Forms.TextBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
@@ -48,12 +48,11 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.textBox1.BackColor = System.Drawing.Color.Navy;
             this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox1.Location = new System.Drawing.Point(-4, -2);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Location = new System.Drawing.Point(-3, -2);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(1715, 80);
+            this.textBox1.Size = new System.Drawing.Size(1286, 65);
             this.textBox1.TabIndex = 0;
             // 
             // lblSchoolName
@@ -64,10 +63,9 @@
             this.lblSchoolName.BackColor = System.Drawing.Color.Navy;
             this.lblSchoolName.Font = new System.Drawing.Font("Perpetua Titling MT", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblSchoolName.ForeColor = System.Drawing.Color.White;
-            this.lblSchoolName.Location = new System.Drawing.Point(591, 28);
-            this.lblSchoolName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblSchoolName.Location = new System.Drawing.Point(443, 23);
             this.lblSchoolName.Name = "lblSchoolName";
-            this.lblSchoolName.Size = new System.Drawing.Size(500, 29);
+            this.lblSchoolName.Size = new System.Drawing.Size(394, 23);
             this.lblSchoolName.TabIndex = 1;
             this.lblSchoolName.Tag = "";
             this.lblSchoolName.Text = "Bulacan Polytechnic University";
@@ -78,10 +76,9 @@
             this.llblHome.BackColor = System.Drawing.Color.Navy;
             this.llblHome.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.llblHome.LinkColor = System.Drawing.Color.White;
-            this.llblHome.Location = new System.Drawing.Point(1599, 28);
-            this.llblHome.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.llblHome.Location = new System.Drawing.Point(1199, 23);
             this.llblHome.Name = "llblHome";
-            this.llblHome.Size = new System.Drawing.Size(68, 25);
+            this.llblHome.Size = new System.Drawing.Size(56, 20);
             this.llblHome.TabIndex = 2;
             this.llblHome.TabStop = true;
             this.llblHome.Text = "Home";
@@ -92,33 +89,20 @@
             this.linkLabel1.BackColor = System.Drawing.Color.Navy;
             this.linkLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.linkLabel1.LinkColor = System.Drawing.Color.White;
-            this.linkLabel1.Location = new System.Drawing.Point(1447, 28);
-            this.linkLabel1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.linkLabel1.Location = new System.Drawing.Point(1085, 23);
             this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(80, 25);
+            this.linkLabel1.Size = new System.Drawing.Size(66, 20);
             this.linkLabel1.TabIndex = 3;
             this.linkLabel1.TabStop = true;
             this.linkLabel1.Text = "Sign In";
-            // 
-            // textBox2
-            // 
-            this.textBox2.BackColor = System.Drawing.Color.Blue;
-            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.textBox2.Location = new System.Drawing.Point(392, 308);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(973, 854);
-            this.textBox2.TabIndex = 8;
             // 
             // pictureBox1
             // 
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.pictureBox1.Image = global::gradesBookApp.Properties.Resources.SignIn_Icon;
-            this.pictureBox1.Location = new System.Drawing.Point(1392, 16);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Location = new System.Drawing.Point(1044, 13);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(49, 46);
+            this.pictureBox1.Size = new System.Drawing.Size(37, 37);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 6;
             this.pictureBox1.TabStop = false;
@@ -127,10 +111,9 @@
             // 
             this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.pictureBox2.Image = global::gradesBookApp.Properties.Resources.Home_Icon;
-            this.pictureBox2.Location = new System.Drawing.Point(1544, 16);
-            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox2.Location = new System.Drawing.Point(1158, 13);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(49, 46);
+            this.pictureBox2.Size = new System.Drawing.Size(37, 37);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 5;
             this.pictureBox2.TabStop = false;
@@ -143,31 +126,39 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.pictureBox3.Image = global::gradesBookApp.Properties.Resources.school_background;
-            this.pictureBox3.Location = new System.Drawing.Point(-4, 69);
-            this.pictureBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox3.Location = new System.Drawing.Point(-3, 56);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(1715, 1093);
+            this.pictureBox3.Size = new System.Drawing.Size(1286, 888);
             this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox3.TabIndex = 7;
             this.pictureBox3.TabStop = false;
             this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
             // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.Blue;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox2.Location = new System.Drawing.Point(294, 250);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(730, 694);
+            this.textBox2.TabIndex = 8;
+            // 
             // textBox3
             // 
             this.textBox3.BackColor = System.Drawing.Color.Navy;
             this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.textBox3.Location = new System.Drawing.Point(468, 235);
-            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox3.Location = new System.Drawing.Point(351, 191);
             this.textBox3.Multiline = true;
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(822, 926);
+            this.textBox3.Size = new System.Drawing.Size(617, 753);
             this.textBox3.TabIndex = 9;
             // 
             // LandingPage
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1712, 1158);
+            this.ClientSize = new System.Drawing.Size(1028, 609);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.pictureBox1);
@@ -177,7 +168,6 @@
             this.Controls.Add(this.lblSchoolName);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.pictureBox3);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "LandingPage";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.LandingPage_Load);

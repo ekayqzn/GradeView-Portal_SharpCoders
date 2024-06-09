ac193f356f7569838ee36fb1012cb2e436465ada
evoraQzone <erikaquizon04@outlook.com>
2024-06-09 15:54:16 +0800

    debug

--- Add Class.cs
+++ Add Class.cs
@@ -13,10 +13,12 @@ namespace gradesBookApp
 {
     public partial class Add_Class : Form
     {
-        public static string classCode;
+        public static string courseCode;
         databaseConnection db = new databaseConnection();
         public int courseId;
         public int classId;
+        public string subjectCode;
+        public string[] subjectCodeE;
         public string studentId = LogInOperation.userID.Trim();
         public Add_Class()
         {
@@ -25,8 +27,9 @@ namespace gradesBookApp
 
         private void rbtnAddClass_Click(object sender, EventArgs e)
         {
-            classCode = txtClassCode.Text.Trim();
-            if(!String.IsNullOrEmpty(classCode))
+            courseCode = txtClassCode.Text.Trim();
+
+            if (!String.IsNullOrEmpty(courseCode))
             {
                 //check if the code exist by retrieving course_id
                 try
@@ -36,7 +39,7 @@ namespace gradesBookApp
                     db.cmd.CommandText = "SELECT course_id, class_id FROM modern_gradesbook.course WHERE course_code = @courseCode";
 
                     db.cmd.Parameters.Clear();
-                    db.cmd.Parameters.AddWithValue("@courseCode", classCode);
+                    db.cmd.Parameters.AddWithValue("@courseCode", courseCode);
                     db.dta.SelectCommand = db.cmd;
 
                     DataTable dataTable = new DataTable();
@@ -50,93 +53,142 @@ namespace gradesBookApp
                     }
                     else //code exist
                     {
+                        
                         courseId = Convert.ToInt32(dataTable.Rows[0]["course_id"]);
                         classId = Convert.ToInt32(dataTable.Rows[0]["class_id"]);
-                        MessageBox.Show(courseId.ToString() + Environment.NewLine + classId.ToString());
-                        //Get subject code
-                        try
+                        
+                        //MessageBox.Show(courseId.ToString() + Environment.NewLine + classId.ToString());
+
+                        try //check if the student is already enrolled in the specific subject
                         {
                             db.Connect();
                             db.cmd.Connection = db.conn;
-                            db.cmd.CommandText = "SELECT subject_code FROM modern_gradesbook.class WHERE class_id = @classID";
+                            db.cmd.CommandText = "SELECT enroll_id FROM modern_gradesbook.enroll WHERE student_id = @studentID AND class_id = @classID";
 
                             db.cmd.Parameters.Clear();
+                            db.cmd.Parameters.AddWithValue("@studentID", LogInOperation.userID);
                             db.cmd.Parameters.AddWithValue("@classID", classId);
 
                             db.dta.SelectCommand = db.cmd;
-                            DataTable dataTable1 = new DataTable();
-                            db.dta.Fill(dataTable1);
+                            DataTable dataTable2 = new DataTable();
+                            db.dta.Fill(dataTable2);
 
-                            //student can't enroll in the same subject at the same time
-                            if(dataTable1.Rows.Count == 0)
+                            if (dataTable2.Rows.Count == 0) //student not enrolled yet to specific class
                             {
-                                try //check if the student is already enrolled in the course
+
+                                //check if the code is not the same subject as the student already enrolled to
+     
+                                try //get the subject code of the given course_code
                                 {
                                     db.Connect();
                                     db.cmd.Connection = db.conn;
-                                    db.cmd.CommandText = "SELECT enroll_id FROM modern_gradesbook.enroll WHERE student_id = @studentID AND class_id = @classID";
+                                    db.cmd.CommandText = "SELECT cl.subject_code AS subject_code FROM modern_gradesbook.class cl JOIN course c ON cl.class_id = c.class_id WHERE c.course_code = @courseCode";
 
                                     db.cmd.Parameters.Clear();
-                                    db.cmd.Parameters.AddWithValue("@studentID", Student_Login.userID);
-                                    db.cmd.Parameters.AddWithValue("@classID", classId);
+                                    db.cmd.Parameters.AddWithValue("@courseCode", courseCode);
 
                                     db.dta.SelectCommand = db.cmd;
-                                    DataTable dataTable2 = new DataTable();
-                                    db.dta.Fill(dataTable2);
-
-                                    if (dataTable2.Rows.Count == 0) //student not enrolled yet
+                                    DataTable subCode = new DataTable();
+                                    db.dta.Fill(subCode);
+                                    if (subCode.Rows.Count > 0)
                                     {
-                                        //add student info to enroll
-                                        try
-                                        {
-                                            db.Connect();
-                                            db.cmd.Connection = db.conn;
-                                            db.cmd.CommandText = "INSERT INTO modern_gradesbook.enroll(student_id, program_id, class_id) SELECT @studentID, program_id, class_id FROM modern_gradesbook.course WHERE course_id = @courseID";
-
-                                            db.cmd.Parameters.Clear();
-                                            db.cmd.Parameters.AddWithValue("@studentID", studentId);
-                                            db.cmd.Parameters.AddWithValue("@courseID", courseId);
-
-                                            if (db.cmd.ExecuteNonQuery() > 0)
-                                            {
-                                                if (MessageBox.Show("Subject Added to Dashboard", "Added", MessageBoxButtons.OK, MessageBoxIcon.Information) == DialogResult.OK)
-                                                {
-                                                    this.Hide();
-                                                    Student_s_Dashboard studentDB = new Student_s_Dashboard();
-                                                    studentDB.ShowDialog();
-                                                    this.Close();
-                                                }
-                                            }
-                                        }
-                                        catch (Exception ex)
-                                        {
-                                            MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                                        }
-                                        finally
-                                        {
-                                            db.Disconnect();
-                                        }
-                                    }
-                                    else
-                                    {
-                                        //Student is already enrolled in specific subject in the same teacher
-                                        MessageBox.Show("You are already enrolled in that subject", "Already Enrolled", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                                            subjectCode = subCode.Rows[0]["subject_code"].ToString();
                                     }
                                 }
                                 catch (Exception ex)
                                 {
-                                    MessageBox.Show(ex.Message);
+                                    MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                                 }
                                 finally
                                 {
                                     db.Disconnect();
                                 }
+
+                                try //get the subject code of the given student_id in enroll
+                                {
+                                    db.Connect();
+                                    db.cmd.Connection = db.conn;
+                                    db.cmd.CommandText = "SELECT cl.subject_code AS subject_code FROM class cl JOIN enroll e ON cl.class_id = e.class_id WHERE e.student_id = @studentID";
+
+                                    db.cmd.Parameters.Clear();
+                                    db.cmd.Parameters.AddWithValue("@studentID", LogInOperation.userID);
+
+                                    db.dta.SelectCommand = db.cmd;
+                                    DataTable subCodeEnroll = new DataTable();
+                                    db.dta.Fill(subCodeEnroll);
+                                    subjectCodeE = new string[subCodeEnroll.Rows.Count];
+                                    for (int i = 0; i < subCodeEnroll.Rows.Count; i++)
+                                    {
+                                        subjectCodeE[i] = subCodeEnroll.Rows[i]["subject_code"].ToString();
+                                    }
+                                }
+                                catch (Exception ex)
+                                {
+                                    MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                                }
+                                finally
+                                {
+                                    db.Disconnect();
+                                }
+
+                                bool isEnrolled = false;
+
+                                for(int i = 0; i < subjectCodeE.Length; i++)
+                                {
+                                    if(subjectCode == subjectCodeE[i])
+                                    {
+                                        isEnrolled = true;
+                                        break;
+                                    }
+                                    else
+                                    {
+                                        isEnrolled = false;
+                                    }
+                                }
+
+                                if(isEnrolled)
+                                {
+                                    MessageBox.Show("You are already enrolled in this subject in different teacher", "Already Enrolled", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                                }
+                                else
+                                {
+                                    //add student info to enroll
+                                    try
+                                    {
+                                        db.Connect();
+                                        db.cmd.Connection = db.conn;
+                                        db.cmd.CommandText = "INSERT INTO modern_gradesbook.enroll(student_id, program_id, class_id) SELECT @studentID, program_id, class_id FROM modern_gradesbook.course WHERE course_id = @courseID";
+
+                                        db.cmd.Parameters.Clear();
+                                        db.cmd.Parameters.AddWithValue("@studentID", studentId);
+                                        db.cmd.Parameters.AddWithValue("@courseID", courseId);
+
+                                        if (db.cmd.ExecuteNonQuery() > 0)
+                                        {
+                                            if (MessageBox.Show("Subject Added to Dashboard", "Added", MessageBoxButtons.OK, MessageBoxIcon.Information) == DialogResult.OK)
+                                            {
+                                                this.Hide();
+                                                Student_s_Dashboard studentDB = new Student_s_Dashboard();
+                                                studentDB.ShowDialog();
+                                                this.Close();
+                                            }
+                                        }
+                                    }
+                                    catch (Exception ex)
+                                    {
+                                        MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                                    }
+                                    finally
+                                    {
+                                        db.Disconnect();
+                                    }
+                                }
                             }
                             else
                             {
-                                MessageBox.Show("The code you input appears to be the same subject you are currently enrolled. Students can't enroll in the same subject at the same time");
+                                //Student is already enrolled in specific subject in the same teacher
+                                MessageBox.Show("You are already enrolled in that subject", "Already Enrolled", MessageBoxButtons.OK, MessageBoxIcon.Error);
                             }
-
                         }
                         catch (Exception ex)
                         {

448fdd8be5687e32d15c27463f636abc97420e93
evoraQzone <erikaquizon04@outlook.com>
2024-06-15 12:38:43 +0800

    added functionality to student module

--- Student's Dashboard.cs
+++ Student's Dashboard.cs
@@ -7,12 +7,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Collections.Specialized.BitVector32;
 
 namespace gradesBookApp
 {
     public partial class Student_s_Dashboard : Form
     {
         databaseConnection db = new databaseConnection();
+        public int classId = 0;
+
+        public static int? mActivityID = null;
+        public static int? mAssignmentID = null;
+        public static int? mLongQuizID = null;
+        public static int? mQuizID = null;
+        public static int? mRecitationID = null;
+        public static int? mExamID = null;
+        public static int? mProjectID = null;
+
+        public static int? fActivityID = null;
+        public static int? fAssignmentID = null;
+        public static int? fLongQuizID = null;
+        public static int? fQuizID = null;
+        public static int? fRecitationID = null;
+        public static int? fExamID = null;
+        public static int? fProjectID = null;
         public Student_s_Dashboard()
         {
             InitializeComponent();
@@ -33,7 +51,7 @@ namespace gradesBookApp
                 //get class_id and subject code that the teacher teaches
                 db.Connect();
                 db.cmd.Connection = db.conn;
-                db.cmd.CommandText = "SELECT s.subject_code, s.subject_name, CONCAT(t.teacher_fname, ' ', t.teacher_lname) AS teacher_name " +
+                db.cmd.CommandText = "SELECT c.class_id, s.subject_code, s.subject_name, CONCAT(t.teacher_fname, ' ', t.teacher_lname) AS teacher_name " +
                     "FROM modern_gradesbook.enroll e JOIN modern_gradesbook.class c ON e.class_id = c.class_id " +
                     "JOIN modern_gradesbook.subject_info s ON s.subject_code = c.subject_code " +
                     "JOIN modern_gradesbook.teacher_info t ON t.teacher_id = c.teacher_id " +
@@ -52,6 +70,7 @@ namespace gradesBookApp
                 string[] subjectCode = new string[dataTable.Rows.Count];
                 string[] subjectName = new string[dataTable.Rows.Count];
                 string[] teacherName = new string[dataTable.Rows.Count];
+                int[] classID = new int[dataTable.Rows.Count];
 
 
                 for (int i = 0; i < dataTable.Rows.Count; i++)
@@ -59,6 +78,7 @@ namespace gradesBookApp
                     subjectCode[i] = dataTable.Rows[i]["subject_code"].ToString();
                     subjectName[i] = dataTable.Rows[i]["subject_name"].ToString();
                     teacherName[i] = dataTable.Rows[i]["teacher_name"].ToString();
+                    classID[i] = Convert.ToInt32(dataTable.Rows[i]["class_id"]);
                 }
 
                 //if the database return the details, will dynamically create a tile/button for that specific subject
@@ -103,6 +123,9 @@ namespace gradesBookApp
                             labelLocationY += 212;
                         }
 
+                        // Store class_id in the Tag property
+                        label.Tag = classID[i];
+
                         //Add Event Handler
                         label.Click += lblSubject_Click;
 
@@ -123,12 +146,76 @@ namespace gradesBookApp
 
         private void lblSubject_Click (object sender, EventArgs e)
         {
+            Label clickedLabel = sender as Label;
+            if (clickedLabel != null)
+            {
+                classId = (int)clickedLabel.Tag;
+                //MessageBox.Show("Class ID: " + classId, "Class ID", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
             //Will show student scores for that specific subject (GRADEBOOK)
-            //!Add Query
-            this.Hide();
-            Student_Module studentModule = new Student_Module();
-            studentModule.ShowDialog();
-            this.Close();
+            try
+            {
+                db.Connect();
+                db.cmd.Connection = db.conn;
+
+                // Retrieve IDs
+                db.cmd.CommandText = "SELECT m_activity_id, m_assignment_id, m_longquiz_id, m_quiz_id, m_project_id, m_exam_id, m_recitation_id, f_activity_id, f_assignment_id, f_longquiz_id, f_quiz_id, f_project_id, f_exam_id, f_recitation_id FROM enroll WHERE student_id = @studentID AND class_id = @classID";
+
+                DataTable dtIDs = new DataTable();
+
+                db.cmd.Parameters.Clear();
+                db.cmd.Parameters.AddWithValue("@studentID", LogInOperation.userID);
+                db.cmd.Parameters.AddWithValue("@classID", classId);
+
+                // Log the parameters FOR DEBUG
+                //MessageBox.Show($"Query: {db.cmd.CommandText}\nParameters:\nprogramID: {programID}\nclassID: {Teacher_s_Dashboard.classID}");
+
+                db.dta.SelectCommand = db.cmd;
+                db.dta.Fill(dtIDs);
+
+                // Debugging: Check if data is retrieved
+                if (dtIDs.Rows.Count == 0)
+                {
+                    MessageBox.Show("No student records found for this class.");
+                    return;
+                }
+                else
+                {
+                    for (int i = 0; i < dtIDs.Rows.Count; i++)
+                    {
+                        mActivityID = dtIDs.Rows[i]["m_activity_id"] != DBNull.Value ? Convert.ToInt32(dtIDs.Rows[i]["m_activity_id"]) : (int?)null;
+                        mAssignmentID = dtIDs.Rows[i]["m_assignment_id"] != DBNull.Value ? Convert.ToInt32(dtIDs.Rows[i]["m_assignment_id"]) : (int?)null;
+                        mLongQuizID = dtIDs.Rows[i]["m_longquiz_id"] != DBNull.Value ? Convert.ToInt32(dtIDs.Rows[i]["m_longquiz_id"]) : (int?)null;
+                        mQuizID = dtIDs.Rows[i]["m_quiz_id"] != DBNull.Value ? Convert.ToInt32(dtIDs.Rows[i]["m_quiz_id"]) : (int?)null;
+                        mRecitationID = dtIDs.Rows[i]["m_recitation_id"] != DBNull.Value ? Convert.ToInt32(dtIDs.Rows[i]["m_recitation_id"]) : (int?)null;
+                        mExamID = dtIDs.Rows[i]["m_exam_id"] != DBNull.Value ? Convert.ToInt32(dtIDs.Rows[i]["m_exam_id"]) : (int?)null;
+                        mProjectID = dtIDs.Rows[i]["m_project_id"] != DBNull.Value ? Convert.ToInt32(dtIDs.Rows[i]["m_project_id"]) : (int?)null;
+
+                        fActivityID = dtIDs.Rows[i]["f_activity_id"] != DBNull.Value ? Convert.ToInt32(dtIDs.Rows[i]["f_activity_id"]) : (int?)null;
+                        fAssignmentID = dtIDs.Rows[i]["f_assignment_id"] != DBNull.Value ? Convert.ToInt32(dtIDs.Rows[i]["f_assignment_id"]) : (int?)null;
+                        fLongQuizID = dtIDs.Rows[i]["f_longquiz_id"] != DBNull.Value ? Convert.ToInt32(dtIDs.Rows[i]["f_longquiz_id"]) : (int?)null;
+                        fQuizID = dtIDs.Rows[i]["f_quiz_id"] != DBNull.Value ? Convert.ToInt32(dtIDs.Rows[i]["f_quiz_id"]) : (int?)null;
+                        fRecitationID = dtIDs.Rows[i]["f_recitation_id"] != DBNull.Value ? Convert.ToInt32(dtIDs.Rows[i]["f_recitation_id"]) : (int?)null;
+                        fExamID = dtIDs.Rows[i]["f_exam_id"] != DBNull.Value ? Convert.ToInt32(dtIDs.Rows[i]["f_exam_id"]) : (int?)null;
+                        fProjectID = dtIDs.Rows[i]["f_project_id"] != DBNull.Value ? Convert.ToInt32(dtIDs.Rows[i]["f_project_id"]) : (int?)null;
+                    }
+
+                    this.Hide();
+                    Student_Module studentModule = new Student_Module();
+                    studentModule.ShowDialog();
+                    this.Close();
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred: " + ex.Message + "\n" + ex.StackTrace);
+            }
+            finally
+            {
+                db.Disconnect();
+            }
         }
 
         private void lblLogOut_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
